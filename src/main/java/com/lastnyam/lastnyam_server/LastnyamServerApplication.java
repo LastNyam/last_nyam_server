715472236e49c98d906859ea487b59dc8ef93275
jungbk0808 <jungbk0808@gmail.com>
2024-11-05 14:46:03 +0900

    init: 프로젝트 초기 설정

---
+++ src/main/java/com/lastnyam/lastnyam_server/LastnyamServerApplication.java
@@ -0,0 +1,13 @@
+package com.lastnyam.lastnyam_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LastnyamServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LastnyamServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lastnyam/lastnyam_server/LastnyamServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lastnyam.lastnyam_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LastnyamServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
